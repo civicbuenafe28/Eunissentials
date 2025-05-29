35b2c76a6c3a8b5aa20f46c00693796efd1a4853
Carl Victor A. Buenafe <152045070+civicbuenafe28@users.noreply.github.com>
2025-05-29 19:21:24 +0800

    Add files via upload

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <file_selector_windows/file_selector_windows.h>
+#include <objectbox_sync_flutter_libs/objectbox_sync_flutter_libs_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  ObjectboxSyncFlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ObjectboxSyncFlutterLibsPlugin"));
+}
